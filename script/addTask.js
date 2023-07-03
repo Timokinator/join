4b7fad60ad051d009184d07236a959f28e28eade
Timo Kempf <124527843+Timokinator@users.noreply.github.com>
2023-07-03 12:03:04 +0200

    Add Task optimized

--- script/addTask.js
+++ script/addTask.js
@@ -24,3 +24,19 @@ async function safeTasks() {
 async function loadTasks() {
    tasks = JSON.parse(await getItem('task_array'));
 };
+
+
+function setPrioValue(prio) {
+    let prioValue = document.getElementById('prio_hidden').value;
+    prioValue = prio;
+    let selectedButton = document.getElementById('prio_btn_'+prio)
+    resetPrioValue();
+    selectedButton.classList.add('prio-selected')
+};
+
+
+function resetPrioValue() {
+    document.getElementById('prio_btn_urgent').classList.remove('prio-selected')
+    document.getElementById('prio_btn_medium').classList.remove('prio-selected')
+    document.getElementById('prio_btn_low').classList.remove('prio-selected')
+};
