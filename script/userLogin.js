ec84839658bd6277b71efa2a1335b6f2b1623cf4
Filip Filipovic <filipfilipo67@gmail.com>
2023-07-04 20:28:10 +0200

    Added first Js code to log in

---
+++ script/userLogin.js
@@ -0,0 +1,19 @@
+import {setItem} from '../script/storage.js';
+import {getItem} from '../script/storage.js';
+
+let users = [];
+
+
+async function register() {
+    let name = document.getElementById('inputName');
+    let email = document.getElementById('inputEmail');
+    let password = document.getElementById('inputPassword');
+
+    users.push({
+        email: email.value,
+        password: password.value,
+        name: name.value
+    })
+
+}
+
