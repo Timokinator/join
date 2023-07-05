b08e0aa4da6bd51b36083bc99e528c61543d8649
Timo Kempf <124527843+Timokinator@users.noreply.github.com>
2023-07-05 13:44:34 +0200

    board 13:44

--- script/script_board.js
+++ script/script_board.js
@@ -1,4 +1,4 @@
-
+let stati = ['todo', 'progress', 'awaiting', 'done'];
 
 
 
@@ -7,8 +7,62 @@ function findTask() {
     let search = document.getElementById('search_input_board');
 
 
-
     console.log(search.value)
     search.value = '';
 
 };
+
+
+async function initBoard() {
+    await loadTasks();
+    renderTasksBoard();
+
+
+    console.log('Test')
+
+};
+
+
+
+function renderTasksBoard() {
+
+    for (let i = 0; i < stati.length; i++) {
+        const status = stati[i];
+
+        let content = document.getElementById('container_tasks_board_' + status);
+        content.innerHTML = '';
+
+        console.log(content)
+
+        for (let j = 0; j < tasks.length; j++) {
+            const task = tasks[j];
+
+            if (task['status'] == status) {
+
+             content.innerHTML += templateSingleTask(task)
+
+            }
+
+
+        }
+
+    }
+
+}
+
+function templateSingleTask(task) {
+    return /*html*/`
+        <div class="single-task">
+            ${task['title']}
+
+
+
+
+        </div>
+        
+        
+
+    `
+
+
+}
