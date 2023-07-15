cf8478dc111ba1c06c5372bd112c7c0f94297096
Timo Kempf <124527843+Timokinator@users.noreply.github.com>
2023-07-16 00:50:03 +0200

    added submenu on user-img

--- script/script_frame.js
+++ script/script_frame.js
@@ -16,3 +16,46 @@ async function includeHtml() {
 function toSummary() {
     window.location.href='../html/summary.html'
 };
+
+
+function openSubmenu(resolution) {
+    let content = document.getElementById('container_submenu_user');
+    content.classList.remove('d-none');
+
+    if (resolution == 'desktop') {
+    content.innerHTML = renderSubmenuDesktop();
+    } else if (resolution == 'mobile') {
+    content.innerHTML = renderSubmenuMobile();
+    setTimeout(() => {
+        closeSubmenu();
+    }, 2500);
+    };
+};
+
+
+function renderSubmenuDesktop() {
+    return /*html*/`
+        <div onclick="closeSubmenu()" class="submenu">
+            <a href="../html/index.html">Log out</a>
+        </div>
+    `;
+};
+
+
+function renderSubmenuMobile() {
+    return /*html*/`
+        <div class="submenu">
+            <a href="../html/help.html">Help</a>
+            <a href="../html/legal_notice.html">Legal Notice</a>
+            <a href="../html/index.html">Log out</a>
+        </div>
+    `;
+};
+
+
+function closeSubmenu() {
+    let content = document.getElementById('container_submenu_user');
+    content.classList.add('d-none');
+};
+
+
